1f76d9bf158d7ccdeed05fe5bfbac781b8e0f756
rk2810 <rohan.kumquat@gmail.com>
2017-09-21 19:57:32 +0530

    Linked list basic program

---
+++ linkedlist.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+// node declaration
+
+struct Node{
+	int data;
+	struct Node *next;
+};
+
+// function to print nodes
+void printList(struct Node *n){
+	while(n!=NULL){
+		printf("%d\n",n->data);
+		n=n->next;
+	}
+}
+
+// lets make 3 nodes
+int main(){
+	struct Node* head=NULL;
+	struct Node* second=NULL;
+	struct Node* third=NULL;
+
+	// allocate 3 blank nodes
+
+	head=(struct Node*)malloc(sizeof(struct Node));
+	second=(struct Node*)malloc(sizeof(struct Node));
+	third=(struct Node*)malloc(sizeof(struct Node));
+// Assign head
+	head->data=1234;
+	head->next=second;
+// Assign second
+	second->data=5678;
+	second->next=third;
+// Assign third
+	third->data=91011;
+	third->next=NULL;
+printList(head);
+return 0;
+}
