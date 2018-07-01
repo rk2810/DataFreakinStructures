c4cfc2c884cd34a318bc2d67391feb31d1f583a5
rk2810 <rohan.kumquat@gmail.com>
2018-07-01 19:15:42 +0530

    Doubly linked list.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

---
+++ DoublyLinkedList/doubly_linked_list.go
@@ -0,0 +1,67 @@
+package main
+
+type Node struct {
+	value int
+	next *Node
+	prev *Node
+}
+
+type List struct {
+	head *Node
+	tail *Node
+}
+
+func(l *List) First() *Node {
+	return l.head
+}
+
+func (l *List) Last() *Node {
+	return l.tail
+}
+
+func (l *List) Push(value int) {
+	node := &Node{value: value}
+	if l.head == nil{
+			l.head = node
+	} else{
+		l.tail.next = node
+		node.prev = l.tail
+	}
+
+	l.tail = node
+}
+
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+func (n *Node) Prev() *Node {
+	return n.prev
+}
+
+func main() {
+	l := &List{}
+	l.Push(1)
+	l.Push(2)
+	l.Push(3)
+
+	n := l.First()
+	for{
+		println(n.value)
+		n = n.Next()
+		if n == nil{
+			break
+		}
+	
+	}
+
+	n = l.Last()
+	for{
+		println(n.value)
+		n = n.Prev()
+		if n == nil{
+			break
+		}
+	}
+
+}
